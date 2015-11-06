bf53190c5cc203fbb609101409f32b11ba69ebb0
wzy822317098 <joey.w.cn@gmail.com>
2015-11-06 12:53:20 +0800

    init

---
+++ library/src/main/java/com/demo/library/MRefreshLayout.java
@@ -0,0 +1,20 @@
+package com.demo.library;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+
+/**
+ * @author wangzy
+ * @desciption
+ * @date 2015/11/6. 12:51
+ */
+public class MRefreshLayout extends SwipeRefreshLayout {
+    public MRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public MRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
